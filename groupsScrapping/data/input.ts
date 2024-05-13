7f00a51956f75daec0aa8fc582151992a703629c
alexismrosales <aleisjorge91@gmail.com>
2024-05-12 22:50:36 -0600

    data updated

--- groupsScrapping/data/input.ts
+++ groupsScrapping/data/input.ts
@@ -1,3 +1,5 @@
-export const programees = [2017, 181, 218, 1556, 1439, 1081, 2016, 1262, 217, 2055]
+export const programees = [2017, 181, 1439, 218, 1556, 1081, 2016, 1262, 217, 2055]
+
+export const programeesNames = ["ACT", "BIO", "CDT", "CC1", "CC2", "FS", "FSB", "MSZC", "MAT", "MATAP"]
 
 export const semester = "20242"
