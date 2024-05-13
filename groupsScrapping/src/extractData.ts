e44e05e94304c40e4f61dfb1ebae0b2b57de6c27
alexismrosales <aleisjorge91@gmail.com>
2024-05-12 22:51:56 -0600

    Extracting groups from page created

--- groupsScrapping/src/extractData.ts
+++ groupsScrapping/src/extractData.ts
@@ -3,9 +3,24 @@ export default class extractData {
   constructor(documents: Array<Document>) {
     this.documents = documents
   }
-  extractProgrameesPages() {
-    const programesLinks: HTMLAnchorElement[] = Array.from(
-      document.querySelectorAll('a.')
-    )
+  private extractGroupsPages() {
+    let allGroups: Array<HTMLAnchorElement[]> = []
+    for (let i = 0; i < this.documents.length; i++) {
+      const groupsNames: HTMLAnchorElement[] = Array.from(
+        this.documents[i].querySelectorAll('div.v-card__text')
+      )
+      allGroups.push(groupsNames)
+    }
+    return allGroups
+  }
+
+  getGroupsByProgramee() {
+    const groups = this.extractGroupsPages()
+    const groupsText = groups.map((groupsByProgramee) => {
+      return groupsByProgramee.map((course) => {
+        return course.textContent?.trim().replace('\n', '').replace(',', '').slice(0, -9).trim()
+      })
+    })
+    return groupsText
   }
 }
--- groupsScrapping/src/index.ts
+++ groupsScrapping/src/index.ts
@@ -1,17 +1,26 @@
 import { programees, semester } from '../data/input'
 import fetchData from './fetchData'
+import extractData from './extractData'
+import fs from 'fs'
 const main = async () => {
-
-
   let documents = await Promise.all(
     programees.map(async (programee) => {
       return await fetchData(`https://www.fciencias.unam.mx/docencia/horarios/indiceplan/${semester}/${programee}`)
-
     })
   )
-
-  console.log(documents)
+  const extract = new extractData(documents)
+  const groups = extract.getGroupsByProgramee()
+  saveGroups(groups)
 }
 
-main()
+const saveGroups = async (groups: (string | undefined)[][]) => {
+  const groupsAsText = groups.map(row => row.join('\n')).join('\n--\n')
+  try {
+    fs.promises.writeFile('./groups.txt', groupsAsText)
+    console.log("Data saved successfully...")
+  } catch (err) {
+    console.log(err)
+  }
 
+}
+main()
